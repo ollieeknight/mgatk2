0040afba361df424c853065472c3e20a9995b094
Oliver Knight <oliver.c.knight@gmail.com>
2026-07-29 21:32:12 +0200

    trim analysis exports

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,21 +1,5 @@
-"""Analysis and reporting functions"""
+"""Analysis helpers."""
 
 from .qc import QCCalculator
-from .report import (
-    create_coverage_plot,
-    create_depth_vs_fragments_plot,
-    create_transposition_frequency_plot,
-    generate_html_report,
-    generate_scrna_html_report,
-)
 
-__all__ = [
-    # From qc
-    "QCCalculator",
-    # From report
-    "generate_html_report",
-    "generate_scrna_html_report",
-    "create_coverage_plot",
-    "create_transposition_frequency_plot",
-    "create_depth_vs_fragments_plot",
-]
+__all__ = ["QCCalculator"]
