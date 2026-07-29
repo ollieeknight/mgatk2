de4027c8bbc373d0808a95b2674830e4ea1b7783
Oliver Knight <oliver.c.knight@gmail.com>
2026-07-29 21:32:12 +0200

    register paired command

--- src/cli/commands/__init__.py
+++ src/cli/commands/__init__.py
@@ -2,8 +2,9 @@
 
 from .call import call
 from .mask import hardmask_fasta
+from .paired import paired
 from .run import run
 from .tenx import tenx
 from .wes import wes
 
-__all__ = ["run", "tenx", "call", "hardmask_fasta", "wes"]
+__all__ = ["run", "tenx", "call", "paired", "hardmask_fasta", "wes"]
