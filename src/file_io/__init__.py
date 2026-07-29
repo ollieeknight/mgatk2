9f43d4f261aed1cecb54e6d07b286e9bf7c73eb1
Oliver Knight <oliver.c.knight@gmail.com>
2026-07-29 21:32:13 +0200

    trim writer exports

--- src/file_io/__init__.py
+++ src/file_io/__init__.py
@@ -1,19 +1,13 @@
 """IO module for mgatk2"""
 
 from .barcode_extraction import extract_barcodes_from_bam
-from .formats import write_cell_stats, write_position_stats, write_run_summary
-from .writers import (
-    IncrementalHDF5Writer,
-    IncrementalTextWriter,
-    write_parameters_json,
-)
+from .formats import write_cell_stats, write_run_summary
+from .writers import IncrementalHDF5Writer, IncrementalTextWriter
 
 __all__ = [
     "IncrementalHDF5Writer",
     "IncrementalTextWriter",
-    "write_parameters_json",
     "write_cell_stats",
-    "write_position_stats",
     "write_run_summary",
     "extract_barcodes_from_bam",
 ]
