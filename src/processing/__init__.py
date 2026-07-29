ee109e878f1efdddc0b9dbf1c8e31c7ee0bb8653
Oliver Knight <oliver.c.knight@gmail.com>
2026-07-29 21:32:13 +0200

    trim processing exports

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -5,11 +5,8 @@ from .processors import CellProcessor, process_barcode_worker
 from .readers import BAMReader
 
 __all__ = [
-    # From readers
     "BAMReader",
-    # From processors
     "CellProcessor",
     "process_barcode_worker",
-    # From pileup
     "PileupGenerator",
 ]
