a3de67877052b17d2f532dfca4e1a5c7f43be633
Oliver Knight <oliver.c.knight@gmail.com>
2026-08-06 22:22:13 +0200

    Export shard scanning API

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -1,12 +1,14 @@
-"""Read processing and pileup generation."""
+"""Read processing and per-cell base counting."""
 
-from .pileup import PileupGenerator
-from .processors import CellProcessor, process_barcode_worker
+from .pileup import ShardResult, plan_shards, scan_shard
+from .processors import build_tasks, process_shards
 from .readers import BAMReader
 
 __all__ = [
     "BAMReader",
-    "CellProcessor",
-    "process_barcode_worker",
-    "PileupGenerator",
+    "ShardResult",
+    "build_tasks",
+    "plan_shards",
+    "process_shards",
+    "scan_shard",
 ]
