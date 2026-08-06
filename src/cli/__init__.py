a7a6cb830db3c77fa6e4dca4ac4fed5b3c0fc676
Oliver Knight <oliver.c.knight@gmail.com>
2026-08-06 22:22:13 +0200

    Unregister wes command

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -3,7 +3,7 @@
 import logging
 
 from .base import cli, main
-from .commands import call, hardmask_fasta, paired, run, tenx, wes
+from .commands import call, hardmask_fasta, paired, run, tenx
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
@@ -11,7 +11,6 @@ cli.add_command(run)
 cli.add_command(tenx)
 cli.add_command(call)
 cli.add_command(paired)
-cli.add_command(wes)
 cli.add_command(hardmask_fasta)
 
 __all__ = ["cli", "main"]
