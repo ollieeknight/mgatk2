2a0352a94b62b036df889a8562f1c16bedec7c42
Oliver Knight <oliver.c.knight@gmail.com>
2026-08-06 22:22:13 +0200

    Drop wes from command exports

--- src/cli/commands/__init__.py
+++ src/cli/commands/__init__.py
@@ -5,6 +5,5 @@ from .mask import hardmask_fasta
 from .paired import paired
 from .run import run
 from .tenx import tenx
-from .wes import wes
 
-__all__ = ["run", "tenx", "call", "paired", "hardmask_fasta", "wes"]
+__all__ = ["run", "tenx", "call", "paired", "hardmask_fasta"]
